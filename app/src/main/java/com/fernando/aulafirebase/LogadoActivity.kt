6af12cd359921170d62f7cb53e6da9d7cda6cb8e
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2024-10-21 11:47:44 -0300

    Desv. métodos salvar, atualizar e remover usuário.

--- app/src/main/java/com/fernando/aulafirebase/LogadoActivity.kt
+++ app/src/main/java/com/fernando/aulafirebase/LogadoActivity.kt
@@ -3,11 +3,13 @@ package com.fernando.aulafirebase
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.fernando.aulafirebase.databinding.ActivityLogadoBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LogadoActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -15,6 +17,10 @@ class LogadoActivity : AppCompatActivity() {
     }
     private val autenticacao by lazy {
         FirebaseAuth.getInstance()
+    }
+
+    private val bancoDados by lazy{
+        FirebaseFirestore.getInstance()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,5 +37,110 @@ class LogadoActivity : AppCompatActivity() {
             autenticacao.signOut()
             startActivity(Intent(this,MainActivity::class.java))
         }
+
+        binding.btnSalvar.setOnClickListener{
+            salvarUsuario()
+        }
+
+        binding.btnAtualizar.setOnClickListener{
+            atualizarUsuario()
+        }
+
+        binding.btnRemover.setOnClickListener{
+            val idUsuarioAtual = autenticacao.currentUser?.uid
+            if(idUsuarioAtual!=null){
+                bancoDados.collection("usuarios")
+                    .document(idUsuarioAtual)
+                    .delete()
+                    .addOnSuccessListener {
+                        AlertDialog.Builder(this)
+                            .setTitle("SUCESSO")
+                            .setMessage("Sucesso ao deletar usuário")
+                            .setPositiveButton("OK") { dialog, posicao ->
+
+                            }
+                            .create().show()
+                    }
+                    .addOnFailureListener {
+                        AlertDialog.Builder(this)
+                            .setTitle("ERROR")
+                            .setMessage("Erro ao deletar usuário")
+                            .setPositiveButton("OK") { dialog, posicao ->
+
+                            }
+                            .create().show()
+                    }
+
+            }
+        }
+    }
+
+    private fun atualizarUsuario() {
+        val dados = mapOf(
+            "nomeCompleto" to binding.editNomeCompleto.text.toString(),
+            "telefone" to binding.editTelefone.text.toString()
+        )
+
+        val idUsuarioAtual = autenticacao.currentUser?.uid
+
+        if(idUsuarioAtual!=null) {
+            bancoDados
+                .collection("usuarios")
+                .document(idUsuarioAtual)
+                .update("telefone",binding.editTelefone.text.toString())
+                .addOnSuccessListener {
+                    AlertDialog.Builder(this)
+                        .setTitle("SUCESSO")
+                        .setMessage("Sucesso ao atualizar usuário")
+                        .setPositiveButton("OK") { dialog, posicao ->
+
+                        }
+                        .create().show()
+                }
+                .addOnFailureListener {
+                    AlertDialog.Builder(this)
+                        .setTitle("ERROR")
+                        .setMessage("Erro ao atualizar usuário")
+                        .setPositiveButton("OK") { dialog, posicao ->
+
+                        }
+                        .create().show()
+                }
+        }
+    }
+
+    private fun salvarUsuario(){
+        val dados = mapOf(
+            "nomeCompleto" to binding.editNomeCompleto.text.toString(),
+            "telefone" to binding.editTelefone.text.toString()
+        )
+
+        val idUsuarioAtual = autenticacao.currentUser?.uid
+
+        if(idUsuarioAtual!=null){
+            bancoDados
+                .collection("usuarios")
+                .document(idUsuarioAtual)
+                .set(dados)
+                .addOnSuccessListener {
+                    AlertDialog.Builder(this)
+                        .setTitle("SUCESSO")
+                        .setMessage("Sucesso ao criar usuário")
+                        .setPositiveButton("OK"){dialog,posicao->
+
+                        }
+                        .create().show()
+                }
+                .addOnFailureListener{
+                    AlertDialog.Builder(this)
+                        .setTitle("ERROR")
+                        .setMessage("Erro ao criar usuário")
+                        .setPositiveButton("OK"){dialog,posicao->
+
+                        }
+                        .create().show()
+                }
+        }
+
     }
 }
